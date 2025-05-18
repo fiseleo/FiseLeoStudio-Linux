1a22629dc1bcbf5cb1170d296749dea8adf85662
afiseleo <aaaa50306@gmail.com>
2025-05-19 00:56:42 +0800

    新增 AssetStudio.GTK 專案，包含基本的程式碼結構與主視窗

---
+++ AssetStudio.GTK/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+namespace AssetStudio.GTK {
+    public partial class MainWindow : Window {
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) 
+            : base(builder.GetRawOwnedObject("MainForm"))
+        {
+            builder.Autoconnect(this);
+        }
+
+        // ← 這就是給 Glade 裡 handler="gtk_main_quit" 用的
+        protected void gtk_main_quit(object sender, EventArgs e)
+        {
+            Application.Quit();
+        }
+    }
+}
